39c1a056abc62f3e4e7d6f93c62dc58fbc43fe5f
Raryson Pereira Rost <raryson.rost@gmail.com>
2023-08-28 08:05:15 -0300

    Adicionando o inicio do servidor

---
+++ index.ts
@@ -0,0 +1,33 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const books = new Map<string, any>();
+books.set("1", {
+  id: "1",
+  title: "The Hound of the Baskervilles",
+  author: "Conan Doyle, Arthur",
+});
+books.set("2", {
+    id: "2",
+    title: "1984",
+    author: "George Orwell",
+  });
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  })
+  .get("/book", (context) => {
+    context.response.body = Array.from(books.values());
+  })
+  .get("/book/:id", (context) => {
+    if (books.has(context?.params?.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
